c5ab73abb9a8bd1ace5242f9ac97676a7e8fcf82
fadikamohamed <fadika.mohamed02@gmail.com>
2018-06-23 11:05:43 +0200

    Ajout modif addEventListener

--- assets/JS/fonction.js
+++ assets/JS/fonction.js
@@ -1,3 +1,6 @@
-function changeImage(url,objet) {
-  objet.src=url;
+switchimg = document.getElementById('image1');
+switchimg.addEventListener('mouseover',changeImage);
+
+function changeImage() {
+  img = document.getElementById('image1').src = 'assets/images/image1_2.jpg';
 }
